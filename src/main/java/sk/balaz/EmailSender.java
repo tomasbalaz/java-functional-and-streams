1cf1e4ceaee980d6afea6576cd51cf43f747db95
Tomas Balaz <balaztomas0@gmail.com>
2023-08-22 15:49:16 +0200

    Email sender coded in functional style, introduced annotation @FunctionalInterface

--- src/main/java/sk/balaz/EmailSender.java
+++ src/main/java/sk/balaz/EmailSender.java
@@ -1,5 +1,14 @@
 package sk.balaz;
 
+@FunctionalInterface
 public interface EmailSender {
     void send();
+
+    // Multiple non-overriding abstract methods found in interface sk.balaz.EmailSender
+    // void unsend();
+
+    String bar = "FOO";
+    default void foo() {
+
+    }
 }
--- src/main/java/sk/balaz/Main.java
+++ src/main/java/sk/balaz/Main.java
@@ -2,7 +2,30 @@ package sk.balaz;
 
 public class Main {
     public static void main(String[] args) {
+
+        // OOP style
         EmailSender emailSender = new Hotmail();
         emailSender.send();
+
+        // Anonymous class style
+        EmailSender sender = new EmailSender() {
+            @Override
+            public void send() {
+                System.out.println("Sending email using anonymous server ...");
+            }
+        };
+        sender.send();
+
+
+        // Lambda style
+        EmailSender anonymous = () -> System.out.println("Sending email using anonymous server ...");
+        anonymous.send();
+
+        EmailSender gmail = () -> System.out.println("Sending email using gmail ...");
+        EmailSender hotmail = () -> System.out.println("Sending email using hotmail ...");
+
+        gmail.send();
+        hotmail.send();
+
     }
 }
